52a9ff7549fb9859aba8e7af326297f736267981
lucioweb <lucio.lemos.385@ufrn.edu.br>
2022-09-25 12:57:18 -0300

    Commit

---
+++ shareds/navbar.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">CRUD </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+      <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Fontes
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+            <li><a class="dropdown-item" href="https://cdnjs.com/libraries/font-awesome" target="_blank">font-awesome</a></li>
+            <li><a class="dropdown-item" href="https://datatables.net/" target="_blank">datatables.net</a></li>
+            <li><a class="dropdown-item" href="https://validator.w3.org/" target="_blank">Validando seu HTML</a></li>
+            <li><a class="dropdown-item" href="https://releases.jquery.com/" target="_blank">JQuery CDN</a></li>
+            <li><a class="dropdown-item" href="#">ttt</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Links úteis
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+            <li><a class="dropdown-item" href="https://getbootstrap.com/docs/5.0/getting-started/introduction/" target="_blank">Bootstrap 5.0</a></li>
+            <li><a class="dropdown-item" href="#">PHP</a></li>
+            <li><a class="dropdown-item" href="#">MySQL</a></li> 
+            <li><a class="dropdown-item" href="#">WAMPP</a></li>
+            <li><a class="dropdown-item" href="#">VSCode</a></li>
+            <li><a class="dropdown-item" href="video.php">Tutorial</a></li>           
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
