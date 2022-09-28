5635320a4eea69a6e788beda79280943a279ca0a
lucioweb <lucio.lemos.385@ufrn.edu.br>
2022-09-28 04:13:53 -0300

    m,m,m

--- includes/update.php
+++ includes/update.php
@@ -6,6 +6,7 @@ if (isset($_POST['submit'])) {
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
     $gender = $_POST['gender'];
+    
     $sql= "UPDATE `crud_2` SET `first_name`='$first_name',`last_name`='$last_name',`email`='$email',`gender`='$gender' WHERE id=$id";       
     $result =mysqli_query($conn, $sql);
     if ($result) {
